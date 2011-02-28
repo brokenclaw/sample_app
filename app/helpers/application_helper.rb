1d10eb039102de6146f839929459e784c0fcb123
Andreas Wagner <feomathar@gmx.de>
2011-02-28 21:31:36 +0100

    Added stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def title
+        base_title='Ruby on Rails Tutorial Sample App'
+        if @title.nil?
+            return base_title
+        else
+            return "#{base_title} | #{@title}"
+        end
+    end
 end
